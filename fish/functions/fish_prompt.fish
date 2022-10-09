4d7003959cf89419bc2537a699faf1c2125c458f
Forrest Jacobs <forrestjacobs@gmail.com>
2022-10-09 12:37:48 -0400

    Add hostname

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -7,7 +7,14 @@ function fish_prompt
     set status_text ' '(__fish_print_pipestatus '[' ']' '|' (set_color $fish_color_status) (set_color $fish_color_error) $last_pipestatus)
   end
 
+  set -l host_color "$fish_color_host"
+  if test -n "$TMUX" -o -n "$SSH_CLIENT"
+    set host_color "$fish_color_host_remote"
+  end
+
   echo -n -s \
+    (set_color $host_color) \
+    '@'"$hostname"' ' \
     (set_color $fish_color_cwd) \
     (string replace -r '^'"$HOME"'($|/)' '~$1' "$PWD") \
     "$status_text" \
