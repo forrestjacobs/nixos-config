af04ac692b8c4c41af49e328343d6de9992735a2
Forrest Jacobs <forrestjacobs@gmail.com>
2023-05-04 12:18:37 -0400

    Use non-emoji prompt to work better over Mosh

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -17,5 +17,5 @@ function fish_prompt
     (set_color $fish_color_cwd) \
     (string replace -r '^'"$HOME"'($|/)' '~$1' "$PWD") \
     "$status_text" \
-    (set_color normal) ' 🐠 '
+    (set_color normal) ' $ '
 end
