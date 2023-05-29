b67a1186a573ebc46357283a1e93bc6228a36045
Forrest Jacobs <forrestjacobs@gmail.com>
2023-05-29 12:54:54 -0400

    Hard code fish path on darwin

--- dots/rutherford/default.nix
+++ dots/rutherford/default.nix
@@ -1,18 +1,25 @@
-{ config, lib, pkgs, inputs, ... }: {
+{ config, lib, pkgs, inputs, ... }:
+
+# We're hard-coding the path here to avoid referencing /run/current-system.
+# /run/current-system doesn't exist immediately after boot, which is annoying
+# when MacOS reopens a terminal app on boot and the shell can't be found.
+let fish = "/nix/var/nix/profiles/system/sw/bin/fish";
+
+in {
 
   imports = [
     inputs.home-manager.darwinModules.home-manager
     ../shared
   ];
 
-  environment.shells = [ pkgs.fish ];
+  environment.shells = [ fish ];
 
   services.nix-daemon.enable = true;
 
   users.users.forrest = {
     description = "Forrest Jacobs";
     home = "/Users/forrest";
-    shell = pkgs.fish;
+    shell = fish;
   };
 
 }
