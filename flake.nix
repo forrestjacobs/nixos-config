a1e0e4c2e700320f2259b32bb8ce7ebb12090c17
Forrest Jacobs <forrestjacobs@gmail.com>
2023-06-01 12:59:37 -0400

    yolo update to get faster hm fish config

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     unstable-pkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     darwin = {
-      url = "github:lnl7/nix-darwin/master";
+      url = "github:lnl7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     NixOS-WSL = {
