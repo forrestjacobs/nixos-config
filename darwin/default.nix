ad7f0c91b28f76a78fb28ffa561fcefae8edd926
Forrest Jacobs <forrestjacobs@gmail.com>
2023-06-01 16:01:33 -0400

    Factor out common darwin config

--- darwin/default.nix
+++ darwin/default.nix
@@ -1,6 +1,29 @@
-{ config, lib, pkgs, inputs, ... }: {
+{ config, lib, pkgs, inputs, ... }:
 
-  imports = [ ../shared.nix ];
+# We're hard-coding the path here to avoid referencing /run/current-system.
+# /run/current-system doesn't exist immediately after boot, which is annoying
+# when MacOS reopens a terminal app on boot and the shell can't be found.
+let fish = "/nix/var/nix/profiles/system/sw/bin/fish";
+
+in {
+
+  imports = [
+    ../shared.nix
+    inputs.home-manager.darwinModules.home-manager
+  ];
+
+  environment.shells = [ fish ];
+
+  homebrew = {
+    enable = true;
+    taps = [
+      "homebrew/cask-fonts"
+    ];
+    casks = [
+      "font-sarasa-gothic"
+      "kitty"
+    ];
+  };
 
   home-manager = {
     useGlobalPkgs = true;
@@ -21,5 +44,17 @@
     babelfishPackage = pkgs.babelfish;
     useBabelfish = true;
   };
+
+  services.nix-daemon.enable = true;
+
+  users.users.forrest = {
+    description = "Forrest Jacobs";
+    home = "/Users/forrest";
+    shell = fish;
+  };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
 
 }
--- hosts/rutherford/default.nix
+++ hosts/rutherford/default.nix
@@ -1,41 +1,5 @@
-{ config, lib, pkgs, inputs, ... }:
+{ config, lib, pkgs, inputs, ... }: {
 
-# We're hard-coding the path here to avoid referencing /run/current-system.
-# /run/current-system doesn't exist immediately after boot, which is annoying
-# when MacOS reopens a terminal app on boot and the shell can't be found.
-let fish = "/nix/var/nix/profiles/system/sw/bin/fish";
-
-in {
-
-  imports = [
-    ../../darwin
-
-    inputs.home-manager.darwinModules.home-manager
-  ];
-
-  environment.shells = [ fish ];
-
-  homebrew = {
-    enable = true;
-    taps = [
-      "homebrew/cask-fonts"
-    ];
-    casks = [
-      "font-sarasa-gothic"
-      "kitty"
-    ];
-  };
-
-  services.nix-daemon.enable = true;
-
-  users.users.forrest = {
-    description = "Forrest Jacobs";
-    home = "/Users/forrest";
-    shell = fish;
-  };
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
+  imports = [ ../../darwin ];
 
 }
