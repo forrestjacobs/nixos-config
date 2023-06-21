eff38bad9d42b2e031269c9328f1f0a6d52c5ff8
Forrest Jacobs <forrestjacobs@gmail.com>
2023-06-20 21:57:17 -0400

    Bake in brew shellenv for Apple Silicon to speed up shell init

--- hm/fish/config.fish
+++ hm/fish/config.fish
@@ -16,7 +16,13 @@ end
 if test -x /usr/local/bin/brew
   /usr/local/bin/brew shellenv | source
 else if test -x /opt/homebrew/bin/brew
-  /opt/homebrew/bin/brew shellenv | source
+  # from `brew shellenv`
+  set -gx HOMEBREW_PREFIX "/opt/homebrew";
+  set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+  set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+  set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+  set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+  set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
 end
 
 # bat
