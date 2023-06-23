20e1b671c6c919b961781be513a87e4764c46e31
Forrest Jacobs <forrestjacobs@gmail.com>
2023-06-23 08:51:35 -0400

    Prevent direnv from being sourced twice

--- shared.nix
+++ shared.nix
@@ -6,6 +6,19 @@
         system = final.system;
       };
     })
+    (final: prev: {
+      direnv = pkgs.stdenv.mkDerivation {
+        name = "direnv-wrapped";
+        src = prev.direnv;
+        # Copy everything except for /share/fish
+        # because that results in sourcing direnv twice
+        installPhase = ''
+          mkdir -p $out/share
+          cp -r bin $out/bin
+          cp -r share/man $out/share/man
+        '';
+      };
+    })
   ];
 
 }
