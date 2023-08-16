c90046fcee001cc2d0b0b121aaf4b832bcc2b1d1
Forrest Jacobs <forrestjacobs@gmail.com>
2023-08-16 16:46:53 -0400

    Split out update script, and split up update and reboot units

--- nixos/update.nix
+++ nixos/update.nix
@@ -1,51 +1,72 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+
+let
+  update = pkgs.writeShellApplication {
+    name = "update";
+    runtimeInputs = [
+      pkgs.gitMinimal
+      config.programs.ssh.package
+    ];
+    text = let
+      user-git = "${config.security.sudo.package}/bin/sudo -u forrest ${pkgs.gitMinimal}/bin/git -C /etc/nixos";
+      rebuild = "${config.system.build.nixos-rebuild}/bin/nixos-rebuild";
+    in ''
+      fail() {
+        echo "Error:" "$@" 1>&2
+        exit 1
+      }
+
+      if [ "$EUID" -ne 0 ]; then
+        fail "Must run as root"
+      fi
+
+      if [ "$(${user-git} branch --show-current)" != "main" ]; then
+        fail "/etc/nixos is not on main branch" 1>&2
+      fi
+
+      if ! ${user-git} diff --quiet || ! ${user-git} diff --staged --quiet; then
+        fail "/etc/nixos has local differences" 1>&2
+      fi
+
+      ${user-git} pull
+      ${rebuild} switch
+    '';
+  };
+
+in
+{
 
   options.services.update-system.enable =
     lib.mkEnableOption "Pull dot updates and rebuild system overnight";
 
-  config = lib.mkIf config.services.update-system.enable {
+  config = {
 
-    systemd.services.update-system = {
+    environment.systemPackages = [ update ];
+
+    systemd.services.update-system = lib.mkIf config.services.update-system.enable {
       description = "Pull dot updates and rebuild system";
       startAt = "4:30";
-      path = [
-        pkgs.gitMinimal
-        config.programs.ssh.package
-      ];
       serviceConfig = {
         Type = "oneshot";
-        WorkingDirectory = "/etc/nixos";
+        ExecStart = "${update}/bin/update";
       };
+    };
+
+    # see https://discourse.nixos.org/t/when-is-a-reboot-necessary-after-auto-upgrade/15254/4
+    systemd.services.reboot-if-changed = lib.mkIf config.services.update-system.enable {
+      description = "Reboot for kernel changes";
+      startAt = "5:30";
+      serviceConfig.Type = "oneshot";
       script = let
-        user-git = "${config.security.sudo.package}/bin/sudo -u forrest ${pkgs.git}/bin/git";
-        rebuild = "${config.system.build.nixos-rebuild}/bin/nixos-rebuild";
         readlink = "${pkgs.coreutils}/bin/readlink";
         shutdown = "${config.systemd.package}/bin/shutdown";
       in ''
         set -euo pipefail
 
-        if [ "$(${user-git} branch --show-current)" != "main" ]; then
-          echo "Not on main branch" 1>&2
-          exit 1
-        fi
-
-        if ! ${user-git} diff --quiet || ! ${user-git} diff --staged --quiet; then
-          echo "Detected local differences" 1>&2
-          exit 1
-        fi
-
-        ${user-git} pull
-
-        # Rebuild the same way as system.autoUpgrade
-        # see https://github.com/NixOS/nixpkgs/blob/720e61ed8de116eec48d6baea1d54469b536b985/nixos/modules/tasks/auto-upgrade.nix#L198
-
-        ${rebuild} boot
         booted="$(${readlink} /run/booted-system/{initrd,kernel,kernel-modules})"
-        built="$(${readlink} /nix/var/nix/profiles/system/{initrd,kernel,kernel-modules})"
+        current="$(${readlink} /run/current-system/{initrd,kernel,kernel-modules})"
 
-        if [ "''${booted}" = "''${built}" ]; then
-          ${rebuild} switch
-        else
+        if [ "''${booted}" != "''${current}" ]; then
           ${shutdown} -r +1
         fi
       '';
