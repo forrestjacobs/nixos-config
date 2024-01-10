4134b248fcb9f8d2f32f63e9c17c64504406571f
Forrest Jacobs <forrestjacobs@gmail.com>
2024-01-10 08:52:54 -0500

    Remove unstable packages again

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    unstable-pkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
@@ -10,23 +9,10 @@
     };
   };
 
-  outputs = { self, nixpkgs, unstable-pkgs, flake-utils, home-manager }:
-    let
-      common = { config, lib, pkgs, ... }: {
-        nixpkgs.overlays = [
-          (final: prev: {
-            unstable = import unstable-pkgs {
-              system = final.system;
-              config.allowUnfree = true;
-            };
-          })
-        ];
-      };
-    in
+  outputs = { self, nixpkgs, flake-utils, home-manager }:
     {
       darwinModules.default = { ... }: {
         imports = [
-          common
           ./common
           home-manager.darwinModules.home-manager
           ./darwin
@@ -34,7 +20,6 @@
       };
       nixosModules.default = { ... }: {
         imports = [
-          common
           ./common
           home-manager.nixosModules.home-manager
           ./nixos
