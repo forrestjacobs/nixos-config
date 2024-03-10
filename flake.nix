f5a6d82db4d2e4c96c645158447e3a0cb7150ef5
Forrest Jacobs <forrestjacobs@gmail.com>
2024-03-10 13:17:27 -0400

    Use consistent home-manager in deps

--- flake.nix
+++ flake.nix
@@ -1,14 +1,15 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    agenix = {
-      url = "github:ryantm/agenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     dotfiles.url = "github:forrestjacobs/dotfiles";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
     snowfall-lib = {
       url = "github:snowfallorg/lib";
